68a980296054f5ef9b8503577ff41ba2388018f8
nathanturnis <nturnis2@gmail.com>
2025-04-15 13:35:39 -0500

    Setup eventual use of serving react app from express

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// use after runing npm build to server react app - not needed until deployment
+//app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
