06c49f639742cd895c4926800796efac8ccd8550
nathanturnis <nturnis2@gmail.com>
2025-04-15 17:27:19 -0500

    Added mantine UI and basic navbar

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,10 +1,84 @@
-import { Outlet } from "react-router";
+import {
+  faBriefcase,
+  faConciergeBell,
+  faHouse,
+  faSignHanging,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AppShell, NavLink } from "@mantine/core";
+import { Link, Outlet, useLocation } from "react-router";
 
 export default function Layout() {
+  const location = useLocation();
+
   return (
-    <>
-      <div>Hello</div>
-      <Outlet />
-    </>
+    <AppShell>
+      <AppShell.Navbar p="lg">
+        <NavLink
+          label="For Sale"
+          component={Link}
+          to="/"
+          active={location.pathname === "/"}
+          leftSection={<FontAwesomeIcon icon={faSignHanging} />}
+          styles={{
+            root: {
+              borderRadius: "6px",
+            },
+          }}
+        />
+        <NavLink
+          label="Housing"
+          component={Link}
+          to="/housing"
+          active={location.pathname.startsWith("/housing")}
+          leftSection={<FontAwesomeIcon icon={faHouse} />}
+          styles={{
+            root: {
+              borderRadius: "6px",
+            },
+          }}
+        />
+        <NavLink
+          label="Services"
+          component={Link}
+          to="/services"
+          active={location.pathname.startsWith("/services")}
+          leftSection={<FontAwesomeIcon icon={faConciergeBell} />}
+          styles={{
+            root: {
+              borderRadius: "6px",
+            },
+          }}
+        />
+        <NavLink
+          label="Jobs"
+          component={Link}
+          to="/jobs"
+          active={location.pathname.startsWith("/jobs")}
+          leftSection={<FontAwesomeIcon icon={faBriefcase} />}
+          styles={{
+            root: {
+              borderRadius: "6px",
+            },
+          }}
+        />
+        <NavLink
+          label="Community"
+          component={Link}
+          to="/community"
+          active={location.pathname.startsWith("/community")}
+          leftSection={<FontAwesomeIcon width={16} icon={faUserGroup} />}
+          styles={{
+            root: {
+              borderRadius: "6px",
+            },
+          }}
+        />
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./Layout.tsx";
+import { createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+const theme = createTheme({
+  defaultRadius: "md",
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<App />} />
+            <Route path="/housing" element={<App />} />
+            <Route path="/services" element={<App />} />
+            <Route path="/jobs" element={<App />} />
+            <Route path="/community" element={<App />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   </StrictMode>
 );
